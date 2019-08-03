365f0e059fb633176575e37b6d1d9447b6d9e440
daniel.sena <danielsena04@gmail.com>
2019-08-03 17:55:46 -0300

    Completing test of CRUD operations on User model

--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -4,13 +4,45 @@ import truncate from "../utils/truncate";
 import factory from "../factories";
 
 describe("Testing CRUD operations on User model", () => {
-  beforeEach(async () => {
+  
+  var user;
+  
+  beforeAll(async () => {
     await truncate();
   });
-
+  
   it("Should insert User", async () => {
-    const user = await factory.create("User", {});
-
+    user = await factory.create("User", {});
     expect(user.id).not.toBeNull();
+  });
+
+  it('Should update an User', async () => {
+    const email = faker.internet.email();
+    await user.update({
+      username: email
+    });
+
+    expect(user.username).toBe(email);
+  });  
+
+  it("Should retrieve an User", async () => {
+    const id = user.id;
+    const _user = await User.findOne({
+      where: {
+        id
+      }
+    });
+    expect(_user).not.toBeNull();
+  });
+
+  it("Should delete an User", async () => {
+    const id = user.id;
+    user.destroy();
+    const _user = await User.findOne({
+      where: {
+        id
+      }
+    });
+    expect(_user).toBeNull();
   });
 });
--- __tests__/utils/truncate.js
+++ __tests__/utils/truncate.js
@@ -1,6 +1,7 @@
 import { sequelize } from "../../src/models";
 
 module.exports = () => {
+  console.log("truncating database");
   return Promise.all(
     Object.keys(sequelize.models).map(key => {
       return sequelize.models[key].destroy({ truncate: true, force: true });
